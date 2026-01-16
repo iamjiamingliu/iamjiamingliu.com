0a96a6d913a6806709e063fa1b1679436737dd2a
iamjiamingliu <57842784+iamjiamingliu@users.noreply.github.com>
2026-01-15 23:39:01 -0800

    started everything

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
 
 app = Flask(__name__)
+Bootstrap5(app)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():
+    return render_template('base.html')
+
+@app.route('/experience')
+def experience():
+    return render_template('base.html')
+
+@app.route('/blogs')
+def blogs():
+    return render_template('base.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('base.html')
 
 
 if __name__ == '__main__':
